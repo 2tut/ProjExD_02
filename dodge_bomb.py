e6e25e89c86714ec15ab47c75b97fd368ac5b538
satoshi <c011709059@edu.teu.ac.jp>
2023-10-03 13:59:55 +0900

    bomb Surface の作成

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,6 +1,7 @@
 import sys
-import pygame as pg
 import random
+import pygame as pg
+
 
 WIDTH, HEIGHT = 1600, 900
 
@@ -13,6 +14,15 @@ def main():
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
     clock = pg.time.Clock()
     tmr = 0
+
+    bomb_color = (255, 0, 0)
+    bomb_r = 10
+    bomb_x = random.randint(0, WIDTH)
+    bomb_y = random.randint(0, HEIGHT)
+    bomb_img = pg.Surface((bomb_r*2, bomb_r*2))
+    bomb_img.set_colorkey((0, 0, 0))
+    pg.draw.circle(bomb_img, bomb_color, (bomb_r, bomb_r), bomb_r)
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT:
@@ -20,6 +30,8 @@ def main():
 
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
+        screen.blit(bomb_img, [bomb_x, bomb_y])
+
         pg.display.update()
         tmr += 1
         clock.tick(10)
